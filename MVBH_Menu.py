9436f5d56a54729d8629446f3f40698a7be80626
MeshVoid <meshvoid@gmail.com>
2022-08-26 20:15:14 +0600

    Adding constraints to def and tgt bones, more refinement to scripts, new class, loads of updates.

--- MVBH_Menu.py
+++ MVBH_Menu.py
@@ -6,13 +6,24 @@ class MV_BH_Menu(bpy.types.Menu):
     
     def draw(self, context):
         layout = self.layout
-        layout.operator("wm.open_mainfile")
+        layout.label(text="Hello world!", icon="WORLD_DATA")
+        layout.operator("armature.mv_bonehelper")
+        layout.label(text="DEF Bones", icon="BONE_DATA")
+        layout.label(text="DEF->TGT", icon="BONE_DATA")
+        
+
+def draw_item(self,context):
+     layout = self.layout
+     layout.menu(MV_BH_Menu.bl_idname)
 
 def register():
-     bpy.utils.register_class(MVBoneHelperMenu)
+     bpy.utils.register_class(MV_BH_Menu)
 
 def unregister():
-     bpy.utils.unregister_class(MVBoneHelperMenu)
+     bpy.utils.unregister_class(MV_BH_Menu)
 
 if __name__ == "__main__":
      register()
+     
+    # The menu can also be called from scripts
+     bpy.ops.wm.call_menu(name=MV_BH_Menu.bl_idname)
