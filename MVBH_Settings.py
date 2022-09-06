fd19270850d7024647c1ce36e468a9826d3adfaa
Chingiz Jumagulov aka MeshVoid <35863328+MeshVoid@users.noreply.github.com>
2022-09-06 14:43:36 +0600

    Delete MVBH_Settings.py

--- MVBH_Settings.py
+++
@@ -1,7 +0,0 @@
-# MeshVoid's Bone Helper addon settings
-
-# TODO: Make sure that you can change naming convention of bone names and other parameters
-# by directly changing values in MVBH_Scripts class
-class MVBH_Preferences():
-    def __init__(self):
-        pass
